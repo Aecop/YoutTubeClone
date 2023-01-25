9eef760d7331551bd1dbb3464c0b248d75749f88
Aecop <joonruiz08@gmail.com>
2023-01-25 11:25:59 -0800

    changed index.js location

---
+++ build/precache-manifest.42279666def0401966ecd438b849768f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a21130a1c8f9bdba645e",
+    "url": "/static/css/main.1f374e7b.chunk.css"
+  },
+  {
+    "revision": "a21130a1c8f9bdba645e",
+    "url": "/static/js/main.7f37c232.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "73b2b1dc4451752ea2cf",
+    "url": "/static/js/2.2dfe7173.chunk.js"
+  },
+  {
+    "revision": "38da36bdd5f59a2649c8a05471ca59e6",
+    "url": "/index.html"
+  }
+];
